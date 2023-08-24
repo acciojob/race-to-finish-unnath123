e602b74d8e12f196beed0a182a8fe3d7b23000d8
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-25 00:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for (let i = 1; i <= 5; i++) {
+  promises.push(
+    new Promise((resolve, reject) => {
+      // Simulate some asynchronous operation
+      setTimeout(() => {
+        if (i === 3) {
+          reject(`Promise ${i} was rejected`);
+        } else {
+          resolve(`Promise ${i} was resolved`);
+        }
+      }, 1000); // Simulating 1 second delay
+    })
+  );
+}
+
+
+
+
+
+document.getElementById("output").innerText=Promise.any(promises);
+
